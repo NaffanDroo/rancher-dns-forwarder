f967f5f1695f47a6e0ece9efd9ad6ae24f09b89a
Fredrik Gevert <fredrik@linkredirector.com>
2017-04-01 13:44:08 +0800

    Entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e
 
-cmd="$@"
-
-if [ ${#cmd} -ge 1 ]; then
-	exec "$@"
-else
-	ls /etc/bind/named.conf
-	ls /usr/sbin/named
-	/usr/sbin/named -g -c /etc/bind/named.conf
-fi
+ls /etc/bind/named.conf
+ls /usr/sbin/named
+/usr/sbin/named -g -c /etc/bind/named.conf
